a5b7ad2a1f21dc9a7386a5182cbe50e3deb15bbb
Peter Jones <peterjones@mac.com>
2019-11-13 19:34:58 -0500

    Initial commit

---
+++ src/build/precache-manifest.5186970212600f3afb49c01712a57ba4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d30d3d5557ce30a852f70656f06af715",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca9c6d1950fd1fe04f69",
+    "url": "/static/css/main.01f8efe7.chunk.css"
+  },
+  {
+    "revision": "0ba9d51c0ff7daa48847",
+    "url": "/static/js/2.e3b10e5b.chunk.js"
+  },
+  {
+    "revision": "ca9c6d1950fd1fe04f69",
+    "url": "/static/js/main.23408e18.chunk.js"
+  },
+  {
+    "revision": "35a7acf4fb928453b799",
+    "url": "/static/js/runtime-main.8727d782.js"
+  }
+]);
